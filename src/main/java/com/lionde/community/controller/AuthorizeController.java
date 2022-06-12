0dae7a5144c26a2c4b062523d6c9088b23e7bd4a
lionde <lidondema@qq.com>
2022-06-12 11:46:16 +0800

    前端-引入session和cookie

--- src/main/java/com/lionde/community/controller/AuthorizeController.java
+++ src/main/java/com/lionde/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -21,7 +23,7 @@ public class AuthorizeController {
     private String redirectUri;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code) {
+    public String callback(@RequestParam(name = "code") String code, HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -29,9 +31,14 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getId());
-        System.out.println(user.getName());
-        System.out.println(user.getBio());
-        return "index";
+        if (user != null) {
+            //登陆成功，写cookie和session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            //登录失败，重新登陆
+            return "redirect:/";
+        }
+        //System.out.println(user.getId());
     }
 }
--- src/main/java/com/lionde/community/provider/GithubProvider.java
+++ src/main/java/com/lionde/community/provider/GithubProvider.java
@@ -22,7 +22,7 @@ public class GithubProvider {
         try (Response response = client.newCall(request).execute()) {
             String str = response.body().string();
             String token = str.split("&")[0].split("=")[1];
-            System.out.println(token);
+            //System.out.println(token);
             return token;
         } catch (Exception e) {
             e.printStackTrace();
