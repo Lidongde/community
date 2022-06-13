dd488a1060061ddb086e38e21af931f0e750b055
lionde <lidondema@qq.com>
2022-06-13 14:00:32 +0800

    前端-服务端自定义cookie、设置session实现持久化(使用Github时服务端是客户端，之后)

--- src/main/java/com/lionde/community/mapper/UserMapper.java
+++ src/main/java/com/lionde/community/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package com.lionde.community.mapper;
 import com.lionde.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into tableuser (name, account_id, token, gmt_create, gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from tableuser where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
